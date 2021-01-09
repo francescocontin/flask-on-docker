b3b7bc8b6c9bcb867bda050c295928d5c44f735b
Fracontin <francescocontin@gmail.com>
2021-01-09 15:03:56 +0100

    inserted wedsocket

--- services/web/project/__init__.py
+++ services/web/project/__init__.py
@@ -7,14 +7,19 @@ from flask import (
     send_from_directory,
     request,
     redirect,
-    url_for
+    url_for,
+    render_template
 )
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_socketio import SocketIO
+from flask_socketio import send, emit
 
 app = Flask(__name__)
 app.config.from_object("project.config.Config")
 db = SQLAlchemy(app)
+
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
 
 
 class User(db.Model):
@@ -30,29 +35,13 @@ class User(db.Model):
 
 @app.route("/")
 def hello_world():
-    return jsonify(hello="world")
+    return render_template('index.html')
+
+@socketio.on('connected')
+def test_connect(data):
+    print('Client connect')
+    send(data)
 
 
-@app.route("/static/<path:filename>")
-def staticfiles(filename):
-    return send_from_directory(app.config["STATIC_FOLDER"], filename)
-
-
-@app.route("/media/<path:filename>")
-def mediafiles(filename):
-    return send_from_directory(app.config["MEDIA_FOLDER"], filename)
-
-
-@app.route("/upload", methods=["GET", "POST"])
-def upload_file():
-    if request.method == "POST":
-        file = request.files["file"]
-        filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config["MEDIA_FOLDER"], filename))
-    return f"""
-    <!doctype html>
-    <title>upload new File</title>
-    <form action="" method=post enctype=multipart/form-data>
-      <p><input type=file name=file><input type=submit value=Upload>
-    </form>
-    """
+if __name__ == '__main__':
+    socketio.run(app)
